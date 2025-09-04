a1b485333c00980a5c7b86b51d9feb92d56f3d52
Aheli kar <ahelikar9@gmail.com>
2025-09-04 11:55:45 +0530

    Update queue_array.c

--- queue_array.c
+++ queue_array.c
@@ -1,16 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
 #define size 100
 int front=-1,rear=-1,a[size];
 void front_insert(int z)
 {
-    if(front==0 && rear==size-1)
+    if((rear+1)%size==front)
     {
         printf("\nNo insertion is possible");
     }
-    else if(front ==-1||rear==-1)
+    else if(front ==-1)
     {
-        front=rear=0;
+        front=rear=0; 
         a[front]=z;
     }
     else if(front==0)
@@ -26,88 +33,92 @@ void front_insert(int z)
 }
 void rear_insert(int z)
 {
-    if(front==0||rear==size-1)
+    if((rear+1)%size==front)
     {
         printf("Queue is full\n");
     }
-    else if(front==-1||rear==-1)
-    {
-        rear=0;
-        a[rear]=z;
-    }
-    else if(rear==size-1)
+    else if(front==-1)
     {
         rear=0;
         a[rear]=z;
     }
     else
     {
-        rear++;
+        rear=((rear+1)%size);
         a[rear]=z;
     }
     
     
-    }
+}
 void front_dequeue()
 {
-    if(front==-1 && rear==-1)
+    if(front==-1)
     {
         printf("There is no element inside the queue\n");
     }
-    else if(front ==rear)
+    else
     {
-        printf("The deleted element from front end is %d\n",a[front]);
+                printf("The deleted element from front end is %d\n",a[front]);
+
+        
+    
+    if(front ==rear)
+    {
         front=-1;
         rear=-1;
     }
-    else if (front==size-1)
+    else
     {
-        printf("The deleted element front is %d",a[front]);
-        front=0;
-        
-    }
-    else{
-        printf("The deleted element from front is%d",a[front]);
-        
+        front=(front+1)%size;
     }
 }
+}
+    
+    
 void rear_dequeue()
 {
-    if(front==-1&&rear==-1)
+    if(front==-1)
     {
         printf("\nThere is no element inside the queue");
     }
-    else if(front==rear)
+    else 
     {
-        printf("The deleted element from rear node is %d",a[rear]);
-    }
-    else if(rear==0)
+        printf("The deleted element from rear end is%d\n",a[rear]);
+        if(front==rear)
+        {
+            front=rear=-1;
+        }
+        else if(rear==0)
+        {
+          rear=size-1;  
+        }
+        else
     {
-        printf("The deleted element from rear node is %d",a[rear]);
-        rear=size-1;
-    }
-    else
-    {
-        printf("The deleted element from rear node is %d",a[rear]);
         rear=rear-1;
     }
-    
+    }
     
     
 }
 void display()
 {
-    printf("\nElements inside the double ended queue are ");
-    while(front!=rear)
+    
+    printf("\nElements inside the double-ended queue are: ");
+    if(front == -1)
     {
-        for(int i=0;i<size;i++)
-        {
-            printf("%d",a[i]);
-        }
-        front=(front+1)%size;
+        printf("Queue is empty.\n");
+        return;
     }
-    printf("%d",a[rear]);
+    int i = front;
+    while(i != rear)
+    {
+        printf("%d ", a[i]);
+        i = (i + 1) % size;
+    }
+    printf("%d\n", a[rear]);
 }
+
+
 int main()
 {
     front_insert(-5);
